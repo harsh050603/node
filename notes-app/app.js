04b89b1a597a08c7c4954c4f5cad27ef3ac40f0d
harsh050603 <harshnotaria2003@gmail.com>
2022-11-10 08:25:08 +0530

    final commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -8,15 +8,90 @@ fs.appendFileSync('notes.txt',' My age is 19.')*/
 //const validator = require('validator')
 
 const chalk = require('chalk')
+const { describe, demandOption } = require('yargs')
+const yargs = require('yargs')
+const notes = require('./notes.js') 
 
-const getNotes = require('./notes.js') 
-const msg=getNotes()
-console.log(msg)
-
+//const msg=getNotes()
+//console.log(msg)
 //console.log(validator.isURL('https://mead.io'))
-const greenMsg = chalk.blue.inverse.bold('Success!')
-console.log(greenMsg)
+// const greenMsg = chalk.blue.inverse.bold('Success!')
+// console.log(greenMsg)
+// console.log(process.argv[2])
 
+//const command = process.argv[2]
 
+//console.log(process.argv)
 
+// if(command==='add'){
+//     console.log('Adding Note!')
+// }else if(command==='remove'){
+//     console.log('Removing Note!')
+// }
 
+//customize yargs version
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    builder:{
+        title:{
+            describe:'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+           describe:'Note body',
+            demandOption:true,
+            type:'string'
+        }
+
+    },
+    handler(argv){
+        notes.addNote(argv.title,argv.body)
+    }
+})
+
+//create remove command
+yargs.command({
+    command:'remove',
+    describe:'Remove a note',
+    builder:{
+        title:{
+            describe:'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.removeNote(argv.title)
+    }
+})
+//list command
+yargs.command({
+    command:'list',
+    describe:'List your notes',
+    handler(){
+    notes.listNotes()
+    }
+})
+
+//read command
+yargs.command({
+    command:'read',
+    describe:'Read a note',
+    builder:{
+        title:{
+            describe:'Note Title',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler(argv){
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
